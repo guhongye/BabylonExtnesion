708dd1d269e5f1d08fee858607a9e9fa8f5ce790
guho <hongye.gu@autodesk.com>
2017-11-30 21:44:40 +0800

    init a sample base on babylon

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+var babylonjs = require('babylonjs');
+var paths = {
+  pages: ['src/*.html']
+};
+
+gulp.task('copyHtml', function() {
+  return gulp.src(paths.pages)
+     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['copyHtml'], function() {
+  return browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/app.ts'],
+    cache: {},
+    packageCache: {}
+  })
+  .plugin(tsify)
+  .transform('babelify', {
+    presets: ['es2015'],
+    extensions: ['.ts']
+  })
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(buffer())
+  .pipe(sourcemaps.init({loadMaps:true}))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('dist'));
+});
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import { Viewer } from "./viewer";
+window.onload = () => {
+  var canvas = document.getElementById("renderCanvas");
+  var main = new Viewer(canvas);
+  
+};
